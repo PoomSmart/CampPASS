25ffe5fb8d1afba240e918341471e7186be73839
PoomSmart <poomst@hotmail.com>
2019-01-15 19:34:10 +0700

    Fix null property.

--- resources/views/camps/show.blade.php
+++ resources/views/camps/show.blade.php
@@ -21,7 +21,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>{{ trans('camp.ShortDescription') }}:</strong>
-                {{ $camp->short_description }}
+                {{ $camp->getShortDescription() }}
             </div>
         </div>
     </div>
