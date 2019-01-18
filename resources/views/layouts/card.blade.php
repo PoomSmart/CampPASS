8c89f8160569fdb9cf87f6e76a0e74f90b7b53f0
PoomSmart <poomst@hotmail.com>
2019-01-18 15:20:32 +0700

    Minor layout adjustment.

--- resources/views/layouts/card.blade.php
+++ resources/views/layouts/card.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('outer_content')
-    <div class="col-lg-12 margin-tb">
-        <div class="float-right">
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-8 margin-tb">
             @yield('button')
         </div>
     </div>
     <div class="row justify-content-center">
-        <div class="col-sm-12">
+        <div class="col-sm-12 col-md-8">
             <div class="card">
                 <div class="card-header">@yield('header')</div>
                 <div class="card-body">
