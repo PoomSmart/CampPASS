492693cfc8409428c32c846f159db03917903c4b
PoomSmart <poomst@hotmail.com>
2019-01-22 10:12:22 +0700

    Update CampApplicationController.php

--- app/Http/Controllers/CampApplicationController.php
+++ app/Http/Controllers/CampApplicationController.php
@@ -12,11 +12,15 @@ class CampApplicationController extends Controller
 {
     public function landing(Camp $camp)
     {
-        // TODO: utilize camper eligibility check
         // TODO: add application date check
-        // TODO: add already-applied state check
-        $eligible = \Auth::user()->isEligibleForCamp($camp);
+        $user = \Auth::user();
+        // TODO: verify camper eligibility check
+        $eligible = $user->isEligibleForCamp($camp);
+        $latest_registration = $camp->registrations()->where('camper_id', $user->id)-latest()->first();
+        // TODO: verify already-applied state check
+        $already_applied = $latest_registration ? $latest_registration->status == RegistrationStatus::QUALIFIED : false;
+        // TODO: verify quota exceed check
         $quota_exceed = $camp->campers(RegistrationStatus::APPROVED)->count() >= $camp->quota;
-        return view('camp_application.landing', compact('camp', 'eligible', 'quota_exceed'));
+        return view('camp_application.landing', compact('camp', 'eligible', 'quota_exceed', 'already_applied'));
     }
 }
