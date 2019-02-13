40f3a08936ef9e90c2318076c4862cd0e7010f33
NutchaH <46516771+pookpiknh@users.noreply.github.com>
2019-02-13 10:31:10 +0700

    profile Show.blade

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -4,14 +4,60 @@
     @lang('account.Name')
 @endsection
 
-    @section('content')
-        <div class="row">
+@section('content')
+    <div class="row">
+        <div class="col-md-4 text-center">
+            <img src="https://via.placeholder.com/150" alt="..." class="img-circle">
+        </div>
+        
+        <div class="col-md-8"> 
+            <h3>Education</h3>
+            <div class="row mb-2">
+                <div class="col-md-6">
+                    <h5>School</h5>
+                    <h6>School A</h6>
+                </div>
+                
+                <div class="col-md-6">
+                    <h5>Program of Study</h5>
+                    <h6>Math Sci</h6>
+                </div>
+            </div>
+            <h3>Camp</h3>
+            <div class="row mb-2">
+                <div class="col-md-6">
+                    <h5>Camper Since</h5>
+                        <h6>1 January 2019</h6>
+                    <h5>Camp Activities</h5>
+                        <h6>Camp A</h6>
+                        <h6>Camp A</h6>
+                        <h6>Camp A</h6>
+                        <h6>Camp A</h6>
+                </div>
+                <div class="col-md-6">
+                    <h5>Camp Joined</h5>
+                    <h6>Math Sci</h6>
+                </div>
+            </div>
+            {{-- Badge --}}
+           <h3>Badge</h3>
+                @foreach ($badges as $badge)
+                     <img src={{ asset("/images/badges/{$badge->getImageName()}.png") }}>
+                @endforeach
+        </div>
+    </div>       
+
+
+
+
+
+        {{-- <div class="row">
             <div class="col-sm-4 text-left">
-                    <img src="https://via.placeholder.com/150" alt="..." class="img-circle">
+                    
             </div>
             
             <div class="col-sm-4">
-                <h2>Education</h2>
+                
                     <h4>School</h4>
                         <p>Some example text some example text. John Doe is an architect and engineer</p>
                     
@@ -35,6 +81,6 @@
                     @foreach ($badges as $badge)
                      <img src={{ asset("/images/badges/{$badge->getImageName()}.png") }}>
                     @endforeach
-                </div>
+                </div> --}}
 
 @endsection
