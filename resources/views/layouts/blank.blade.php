fa58329d30597b0247a5632223bb1ac8831d0adf
PoomSmart <thatchapon.unp@outlook.com>
2019-02-13 19:52:01 +0700

    Layout and profile as it should be.

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -2,13 +2,13 @@
 
 @section('outer_content')
     <div class="row justify-content-center">
-        <div class="col-sm-12 col-md-10 margin-tb">
-            @yield('extra-buttons')
-        </div>
-    </div>
-    <div class="row justify-content-center">
         <div class="col-sm-12 col-md-10">
             @yield('content')
         </div>
+        @if (View::hasSection('extra-buttons'))
+            <div class="col-sm-12 col-md-10 my-4 text-center">
+                @yield('extra-buttons')
+            </div>
+        @endif
     </div>
 @endsection
