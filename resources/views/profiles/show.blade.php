a0005231a4ed95fe24f1ac8dce3b84c8bc446e89
PoomSmart <thatchapon.unp@outlook.com>
2019-02-13 11:20:58 +0700

    Columns for camps.

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -23,20 +23,35 @@
                     <h6>Math Sci</h6>
                 </div>
             </div>
-            <h3>Camp</h3>
+            <h3>Camps</h3>
             <div class="row mb-2">
                 <div class="col-md-6">
                     <h5>Camper Since</h5>
                         <h6>1 January 2019</h6>
-                    <h5>Camp Activities</h5>
-                        <h6>Camp A</h6>
-                        <h6>Camp A</h6>
-                        <h6>Camp A</h6>
-                        <h6>Camp A</h6>
                 </div>
                 <div class="col-md-6">
                     <h5>Camp Joined</h5>
                     <h6>Math Sci</h6>
+                </div>
+                <div class="col-12 mt-2">
+                    <h5>Camp Activities</h5>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h6>Camp A</h6>
+                            </div>
+                            <div class="col-md-6">
+                                <h6>Camp A</h6>
+                            </div>
+                            <div class="col-md-6">
+                                <h6>Camp A</h6>
+                            </div>
+                            <div class="col-md-6">
+                                <h6>Camp A</h6>
+                            </div>
+                            <div class="col-md-6">
+                                <h6>Camp A</h6>
+                            </div>
+                        </div>
                 </div>
             </div>
             {{-- Badges --}}
