c7ccf84c19fea4639407a61cc0d24938a3db87d9
PoomSmart <thatchapon.unp@outlook.com>
2019-02-13 13:41:58 +0700

    Fallback.

--- resources/views/components/dialog.blade.php
+++ resources/views/components/dialog.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalLabel">{{ $title }}</h5>
+                <h5 class="modal-title" id="modalLabel">{{ isset($title) ? $title : trans('app.Confirmation') }}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -13,10 +13,10 @@
             <div class="modal-footer">
                 <form id="confirm-form" action="" method="POST">
                     @csrf
-                    @if ($method == 'DELETE')
+                    @if (isset($method) && $method == 'DELETE')
                         @method('DELETE')
                     @endif
-                    <button type="submit" class="btn btn-{{ isset($confirm_type) ? $confirm_type : 'primary' }}">{{ $confirm_label }}</button>
+                    <button type="submit" class="btn btn-{{ isset($confirm_type) ? $confirm_type : 'primary' }}">{{ isset($confirm_label) ? $confirm_label : trans('app.Confirm') }}</button>
                 </form>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">@lang('app.Close')</button>
             </div>
