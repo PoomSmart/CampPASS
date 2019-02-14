fe40ee056a0d279793f5022b46708616c207ccab
NutchaH <46516771+pookpiknh@users.noreply.github.com>
2019-02-14 15:24:17 +0700

    th

---
+++ resources/lang/en/camp_category.php
@@ -0,0 +1,30 @@
+<?php
+
+return  [
+
+    'Engineering' => 'Engineering',
+    'Science' => 'Science',
+    'Computer' => 'Computer',
+    'Education' => 'Education',
+    'Architectural' => 'Architectural',
+    'Law' => 'Law',
+    'Language-Human' => 'Language-Human',
+    'Commart' => 'Commart',
+    'Health' => 'Health',
+    'Doctor' => 'Doctor',
+    'Nurse' => 'Nurse',
+    'Dentist' => 'Dentist',
+    'Psychology' => 'Psychology',
+    'Pharmacy' => 'Pharmacy',
+    'Music' => 'Music',
+    'Tutor' => 'Tutor',
+    'Account-Economic' => 'Account-Economic',
+    'Social-Science' => 'Social-Science',
+    'Veter' => 'Veter',
+    'Art' => 'Art',
+    'Agri-Fishery' => 'Agri-Fishery',
+    'Political' => 'Political',
+    'Youth' => 'Youth',
+    'Preserve' => 'Preserve',
+
+];
