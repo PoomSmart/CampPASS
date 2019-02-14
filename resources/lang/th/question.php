7d7d4cf06ede02d707feb3ac517c490d6debcf81
NutchaH <46516771+pookpiknh@users.noreply.github.com>
2019-02-14 10:33:11 +0700

    th

---
+++ resources/lang/th/question.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'TEXT' => 'Text',
+    'PARAGRAPH' => 'Paragraph',
+    'CHOICES' => 'Multiple choices',
+    'CHECKBOXES' => 'Multiple checkboxes',
+    'FILE' => 'File upload',
+    'Type' => 'Type',
+    'Graded' => 'Graded',
+    'Question' => 'Question',
+    'CreateQuestions' => 'Create Questions',
+    'EnterQuestionPlaceholder' => 'Enter question here',
+    'AddMoreQuestion' => 'Add More Question',
+    'AddMoreCheckbox' => 'Add More Checkbox',
+    'AddMoreChoice' => 'Add More Choice',
+    'ScoreThreshold' => 'Score Threshold',
+    'EnterThreshold' => 'Enter the threshold, if necessary',
+    'NoFileUploaded'=> 'No file was uploaded.',
+];
