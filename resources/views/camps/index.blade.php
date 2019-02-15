b04552b6e1a4facbbafbbce0b9935f0b8a4ed413
PoomSmart <thatchapon.unp@outlook.com>
2019-02-15 09:34:49 +0700

    A little better width.

--- resources/views/camps/index.blade.php
+++ resources/views/camps/index.blade.php
@@ -28,7 +28,7 @@
             <th class="align-middle">@lang('camper.RegisteredCampers')</th>
             <th class="align-middle">Grading Type</th>
             <th class="align-middle">@lang('camp.Status')</th>
-            <th class="align-middle" width="240px">@lang('app.Actions')</th>
+            <th class="align-middle" width="250px">@lang('app.Actions')</th>
         </thead>
 	    @foreach ($camps as $camp)
 	    <tr>
