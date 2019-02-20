881c7fb9d5fb533054cc08b5891769c568e428b4
NutchaH <46516771+pookpiknh@users.noreply.github.com>
2019-02-20 09:14:51 +0700

    Update passwords.php

--- resources/lang/th/passwords.php
+++ resources/lang/th/passwords.php
@@ -3,7 +3,7 @@
 return [
     'password' => 'รหัสผ่านควรมี 6 ตัวอักษรหรือขึ้นไปและตรงกับรหัสผ่านที่ยืนยัน',
     'reset' => 'เปลี่ยนรหัสผ่านของคุณสำเร็จ!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'sent' => 'เราได้ส่งลิงก์เปลี่ยนรหัสให้ทางอีเมลของคุณแล้ว !',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
 ];
