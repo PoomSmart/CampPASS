7640d5078efa88de7fb46840ea1cb750860664ed
PoomSmart <thatchapon.unp@outlook.com>
2019-02-27 10:28:38 +0700

    Sizing.

--- resources/views/auth/register-landing.blade.php
+++ resources/views/auth/register-landing.blade.php
@@ -7,11 +7,11 @@
 @section('card_content')
     <div class="row">
         <div class="col-md-6 text-center">
-            <img src="{{ asset('images/camper.png') }}" class="w-100" title="{{ trans('account.Camper') }}"/>
+            <img src="{{ asset('images/camper.png') }}" class="w-50" title="{{ trans('account.Camper') }}"/>
             <h2><a href="{{ route('register-camper') }}">@lang('account.Camper')</a></h2>
         </div>
         <div class="col-md-6 text-center">
-            <img src="{{ asset('images/campmaker.png') }}" class="w-100" title="{{ trans('account.CampMaker') }}"/>
+            <img src="{{ asset('images/campmaker.png') }}" class="w-50" title="{{ trans('account.CampMaker') }}"/>
             <h2><a href="{{ route('register-campmaker') }}">@lang('account.CampMaker')</a></h2>
         </div>
     </div>
