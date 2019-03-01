df7e44ece0746bd221a1fd69fc0a26e725173374
NutchaH <46516771+pookpiknh@users.noreply.github.com>
2019-03-01 10:05:35 +0700

    mycamp

--- resources/lang/en/exception.php
+++ resources/lang/en/exception.php
@@ -5,6 +5,16 @@ return [
     'AlreadyAppliedCamp' => 'You already have applied for this camp.',
     'NoQuestion' => 'There are no questions in here.',
     'NoAnswered' => 'You have not answered anything.',
-    ''
+    'ConfirmedAttending' => 'You already confirmed attending:camp.',
+    'FullyQualified' => 'You are fully qualified for',
+    'You cannot withdraw the camp you already confirmed the attendance.',
+    'AlreadyWithdrawed' => "You already withdrawed from:camp.",
+    'WithdrawAttendance.' => 'You cannot withdraw the camp you already confirmed the attendance.',
+    'NoApplicationRank' => 'No application forms to be ranked.',
+    'NoFinalApplicationRank.' => 'No finalized application forms to be ranked.',
+    'AllApplicationFinalRank.' => 'All application forms must be finalized before ranking.',
+    'CandidatesAnnounced' => 'Candidates for this camp are already announced.',
+    'NoCamperAnnounce' => 'There are no campers to announce to.',
+
     
 ];
--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -2,5 +2,7 @@
 
 return [
     'NewAccountCreated' => 'Successfully created a new account. Please check your email and activate your account.',
-    
+    'ErrorDelete' => 'Error deleting the file.',
+    'SuccessDelete' => 'File deleted successfully.',
+
 ];
