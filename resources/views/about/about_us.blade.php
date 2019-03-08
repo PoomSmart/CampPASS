82af17937945d11af10c02648298b873da49495f
PoomSmart <thatchapon.unp@outlook.com>
2019-03-08 12:26:17 +0700

    Generalization.

--- resources/views/about/about_us.blade.php
+++ resources/views/about/about_us.blade.php
@@ -7,7 +7,7 @@
 @section('card_content')
     <div class="row mt-4">
         <div class="col-md-4">
-            <img src="{{ asset('/images/logo.png') }}" alt="CampPASS" class="pb-3 w-100">
+            <img src="{{ asset('/images/logo.png') }}" alt="{{ config('app.name') }}" class="pb-3 w-100">
         </div>
         <div class="col-md-8">
             <h4 class="mb-4">{{ config('app.name') }}</h4>
