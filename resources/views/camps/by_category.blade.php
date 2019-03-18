1e62cb200d5ab5b861bb1264447c7124c7a1df05
PoomSmart <thatchapon.unp@outlook.com>
2019-03-18 18:46:19 +0700

    Bug fix.

--- resources/views/camps/by_category.blade.php
+++ resources/views/camps/by_category.blade.php
@@ -9,27 +9,39 @@
     @endif
 @endsection
 
-@section('custom-width')
-    <div class="col-12 col-xl-10">
-@endsection
+@if (isset($categorized_camps))
+    @section('custom-width')
+        <div class="col-12 col-xl-10">
+    @endsection
 
-@section('sidebar-items')
-    @php $i = 0 @endphp
-    @foreach ($categorized_camps as $category => $camps)
-        <li class="nav-item"><a class="nav-link rounded{{ $i == 0 ? ' active' : '' }}" data-toggle="scroll" href="#{{ $i++ }}"><b>{{ $category }}</b></a></li>
-    @endforeach
-@endsection
+    @section('sidebar-items')
+        @php $i = 0 @endphp
+        @foreach ($categorized_camps as $category => $camps)
+            <li class="nav-item"><a class="nav-link rounded{{ $i == 0 ? ' active' : '' }}" data-toggle="scroll" href="#{{ $i++ }}"><b>{{ $category }}</b></a></li>
+        @endforeach
+    @endsection
+@endif
 
 @section('content')
-    @php $i = 0 @endphp
-    @foreach ($categorized_camps as $category => $camps)
+    @if (isset($categorized_camps))
+        @php $i = 0 @endphp
+        @foreach ($categorized_camps as $category => $camps)
+            <div class="container-fluid mt-4">
+                <h3 class="mb-4 d-inline-block" id="{{ $i++ }}">{{ $category }}</h3>
+                @component('components.card_columns', [
+                    'objects' => $camps,
+                    'component' => 'components.camp_block',
+                ])
+                @endcomponent
+            </div>
+        @endforeach
+    @else
         <div class="container-fluid mt-4">
-            <h3 class="mb-4 d-inline-block" id="{{ $i++ }}">{{ $category }}</h3>
             @component('components.card_columns', [
                 'objects' => $camps,
                 'component' => 'components.camp_block',
             ])
             @endcomponent
         </div>
-    @endforeach
+    @endif
 @endsection
