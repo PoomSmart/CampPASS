42aba6106c40e0c78c4e2467cfa60d177f7842c4
NutchaH <46516771+pookpiknh@users.noreply.github.com>
2019-03-28 11:22:28 +0700

    submitted form

--- resources/views/layouts/submitted_form.blade.php
+++ resources/views/layouts/submitted_form.blade.php
@@ -13,6 +13,47 @@
         <div class="col-9">{{ $user->nickname_en }} ({{ $user->nickname_th }})</div>
         <div class="col-3"><b>Date of Birth:</b></div>
         <div class="col-9">{{ $user->dob }}</div>
+        <div class="col-3"><b>Gender:</b></div>
+        <div class="col-9">{{ $user->gender }}</div>
+        <div class="col-3"><b>Blood Group:</b></div>
+        <div class="col-9">{{ $user->blood_group }}</div>
+        <div class="col-3"><b>Region:</b></div>
+        <div class="col-9">{{ $user->religion_id }}</div>
+    </div>
+    <h3>@lang('account.Education')</h3>
+    <div class="row">
+        <div class="col-3"><b>School:</b></div>
+        <div class="col-9">{{ $user->school_id }}</div>
+        <div class="col-3"><b>Program of Study:</b></div>
+        <div class="col-9">{{ $user->programs }}</div>
+        <div class="col-3"><b>Education Level:</b></div>
+        <div class="col-9">{{ $user->education_level }}</div>
+        <div class="col-3"><b>Cumulative GPA:</b></div>
+        <div class="col-9">{{ $user->cgpa }}</div>
+    </div>
+    <h3>@lang('profile.ContactInformation')</h3>
+    <div class="row">
+        <div class="col-3"><b>Mobile No.:</b></div>
+        <div class="col-9">{{ $user->school_id }}</div>
+        <div class="col-3"><b>Street Address:</b></div>
+        <div class="col-9">{{ $user->street_address }}</div>
+        <div class="col-3"><b>Province:</b></div>
+        <div class="col-9">{{ $user->province }}</div>
+        <div class="col-3"><b>Zip Code:</b></div>
+        <div class="col-9">{{ $user->zipcode }}</div>
+    </div>
+    <h3>@lang('profile.EmergencyContactInformation')</h3>
+    <div class="row">
+        <div class="col-3"><b>Guardian:</b></div>
+        <div class="col-9">{{ $user->guardian_name }} {{ $user->guardian_surname }}</div>
+        <div class="col-3"><b>Mobile No.:</b></div>
+        <div class="col-9">{{ $user->guardian_mobile_no }}</div>
+        <div class="col-3"><b>Role:</b></div>
+        <div class="col-9">{{ $user->guardian_role }}</div>
+    </div>
+    <h3>@lang('app.CampApplicationForm')</h3>
+    <div class="row">
+        
     </div>
 </body>
 </html>
