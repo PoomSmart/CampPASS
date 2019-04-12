f0ae4fdd71480b3d7ca1d69e718bd8412e38a469
PoomSmart <thatchapon.unp@outlook.com>
2019-04-12 14:31:40 +0700

    Improved.

--- resources/views/components/file_upload.blade.php
+++ resources/views/components/file_upload.blade.php
@@ -1,6 +1,8 @@
-<div class="{{ isset($full_width) && $full_width ? 'd-flex' : 'd-inline-flex' }}">
+<div class="{{ isset($full_width) && $full_width ? 'd-flex' : 'd-inline-flex align-items-center' }}">
     @if (isset($download_route))
-        <a class="btn btn-secondary mx-1{{ isset($full_width) && $full_width ? ' w-100' : null }}" href="{{ route($download_route, $args) }}"><i class="far fa-eye mr-2 fa-xs"></i>{{ isset($value) ? $value : trans('app.View') }}</a>
+        <a class="btn btn-secondary mx-1 h-100{{ isset($full_width) && $full_width ? ' w-100' : null }}" href="{{ route($download_route, $args) }}">
+            <i class="far fa-eye mr-2 fa-xs"></i>{{ isset($value) ? $value : trans('app.View') }}
+        </a>
     @endif
     @if (isset($upload) && $upload)
         <label class="btn btn-primary mb-0 mx-1{{ isset($full_width) && $full_width ? ' w-100' : null }}">
@@ -8,15 +10,16 @@
             <input type="file"
                 id="{{ $name }}"
                 name="{{ $name }}"
-                style="max-width:0;padding:0;border:0;"
+                style="max-width:0;max-height:0;padding:0;border:0;"
                 @if (isset($required) && $required)
                     required
                 @endif
-                onchange="this.parentElement.innerText=this.files[0].name{{ isset($auto_upload) && $auto_upload ? ';form.submit()' : null }}">
+                onchange="var t = this.parentElement.nextElementSibling; t.innerText=this.files[0].name; t.hidden=false{{ isset($auto_upload) && $auto_upload ? ';form.submit()' : null }}">
         </label>
+        <label class="ml-2 mb-0" hidden></label>
     @endif
     @if (isset($delete_route) && isset($download_route))
-        <a class="btn btn-danger mx-1{{ isset($full_width) && $full_width ? ' w-100' : null }}" href="{{ route($delete_route, $args) }}"><i class="fas fa-trash-alt mr-2 fa-xs"></i>@lang('app.Delete')</a>
+        <a class="btn btn-danger mx-1 h-100{{ isset($full_width) && $full_width ? ' w-100' : null }}" href="{{ route($delete_route, $args) }}"><i class="fas fa-trash-alt mr-2 fa-xs"></i>@lang('app.Delete')</a>
     @endif
 </div>
 @if (isset($desc))
