b662c5055840e707167e31f4bc805aad52883f07
PoomSmart <thatchapon.unp@outlook.com>
2019-04-24 17:54:30 +0700

    Tweak.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,11 +1,18 @@
+@php $passed = isset($data['passed']) && $data['passed'] @endphp
 <div class="card card-with-shadow-static">
-    <div class="card-header text-center {{ isset($override_header_color) ? $override_header_color : (isset($data['passed']) && $data['passed'] ? 'camppass-orange' : 'bg-secondary text-white') }}">{{ $header }}</div>
+    <div class="card-header text-center {{ isset($override_header_color) ? $override_header_color : ($passed ? 'camppass-orange' : 'bg-secondary text-white') }}">{{ $header }}</div>
     <div class="card-body">
         @if (isset($data['text']))
-            <p class="card-text">{{ $data['text'] }}</p>
+            <p class="card-text{{ !$passed ? ' text-muted' : '' }}">{{ $data['text'] }}</p>
         @endif
         @if (isset($extra_body))
-            {{ $extra_body }}
+            @if (!$passed)
+                <div class="text-muted">
+            @endif
+                {{ $extra_body }}
+            @if (!$passed)
+                </div>
+            @endif
         @endif
         @if (isset($buttons) && isset($data['button']) && $data['button'])
             <div class="d-flex">
