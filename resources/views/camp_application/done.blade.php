ceb3a0ca874a43abbb02f567c6c55264488e7c7f
PoomSmart <thatchapon.unp@outlook.com>
2019-04-24 17:58:53 +0700

    Five.

--- resources/views/camp_application/done.blade.php
+++ resources/views/camp_application/done.blade.php
@@ -20,13 +20,13 @@
         <div class="col-12 mt-2">
             <p class="text-muted">@lang('app.RedirectTo', [
                 'where' => trans('registration.Status'),
-                'seconds' => 2,
+                'seconds' => 5,
             ])</p>
         </div>
     </div>
     <script>
         setTimeout(function() {
             window.location = "{!! route('camp_application.status', $registration->id) !!}"
-        }, 2000);
+        }, 5000);
     </script>
 @endsection
